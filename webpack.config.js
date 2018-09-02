cd2078de320dd9ad1c2a561fc5c26788c778f9f3
Frederic Vogels <frederic.vogels@gmail.com>
2018-09-02 21:08:22 +0200

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+
+module.exports = [
+    {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'index.js',
+            library: '',
+            libraryTarget: 'commonjs'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        'ts-loader',
+                    ],
+                    exclude: /node_modules/
+                },
+             ]
+        },
+        resolve: {
+            extensions: ['.ts' ],
+        },
+    },
+];
